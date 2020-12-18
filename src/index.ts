a9cb9667c1cb3b949daee4766d94270fe8c2801c
DasDarki <rageskilln@gmail.com>
2020-12-18 15:10:52 +0100

    remove test file

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import {PSScraper} from "./PSScraper";
-import {UploadPlan} from "./model/UploadPlan";
-
-(async () => {
-    let plan: UploadPlan = await PSScraper.uploadPlan();
-    console.log(plan);
-})();
